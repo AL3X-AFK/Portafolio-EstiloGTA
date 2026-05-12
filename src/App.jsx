0d534b40736d1d72f27fc0e11ac738182c64a1c9
Marcos <mfraile@e8dsoluciones.es>
2026-05-12 13:14:13 +0200

    plus

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,43 @@
 import React, { useState } from "react";
-import { useGSAP } from '@gsap/react';
+import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 
-import fondo from './assets/bg.png'
+import fondo from "./assets/bg.png";
+import sky from "./assets/sky.png"
+import girl from "./assets/girlbg.png"
+
 
 function App() {
-let [showContent, setShowContent] = useState(false);
-useGSAP(() => {
-  const tl = gsap.timeline();
+  let [showContent, setShowContent] = useState(false);
+  useGSAP(() => {
+    const tl = gsap.timeline();
 
-  tl.to(".vi-mask-group",{
-    rotate: 10,
-    ease: "power4.inOut",
-    duration: 2,
-    transformOrigin: "50% 50%",
-  })
-  .to(".vi-mask-group",{
-    scale: 10,
-    duration: 2,
-    delay: -1.8,
-    ease: "Expo.easeInOut",
-    transformOrigin: "50% 50%",
-    opacity: 0,
-    onUpdate: function(){
-      if(this.progress() >= .9){
-        document.querySelector(".svg").remove();
-        setShowContent(true);
-        this.kill();
-      }
-    },
+    tl.to(".vi-mask-group", {
+      rotate: 10,
+      ease: "power4.inOut",
+      duration: 2,
+      transformOrigin: "50% 50%",
+    }).to(".vi-mask-group", {
+      scale: 10,
+      duration: 2,
+      delay: -1.8,
+      ease: "Expo.easeInOut",
+      transformOrigin: "50% 50%",
+      opacity: 0,
+      onUpdate: function () {
+        if (this.progress() >= 0.9) {
+          document.querySelector(".svg").remove();
+          setShowContent(true);
+          this.kill();
+        }
+      },
+    });
   });
-});
 
   return (
     <>
-      <div className='svg flex items-center justify-center fixed top-0 left-0 z-[100] w-full h-screen overflow-hidden bg-black'>
-        <svg viewBox='0 0 800 600' preserveAspectRatio='xMidYMid slice'>
+      <div className="svg flex items-center justify-center fixed top-0 left-0 z-[100] w-full h-screen overflow-hidden bg-black">
+        <svg viewBox="0 0 800 600" preserveAspectRatio="xMidYMid slice">
           <defs>
             <mask id="viMask">
               <rect width="100%" height="100%" fill="black" />
@@ -58,13 +60,25 @@ useGSAP(() => {
             href={fondo}
             width="100%"
             height="100%"
-            preserveAspectRatio='xMidYMid slice'
-            mask='url(#viMask)'
+            preserveAspectRatio="xMidYMid slice"
+            mask="url(#viMask)"
           />
         </svg>
       </div>
+      {showContent && (
+        <div className="main w-full">
+          <div className="landing w-full h-screen bg-black">
+            <div className="imagesdiv relative w-full h-screen">
+              <img className="absolute top-0 left-0 w-full h-full object-cover" src={sky} alt="" />
+              <img className="absolute top-0 left-0 w-full h-full object-cover z-10" src={fondo} alt="" />
+              <img className="absolute -bottom-[35%] left-1/2 -translate-x-1/2 scale-[1.4] h-full object-cover z-10" src={girl} alt="" />
+
+            </div>
+          </div>
+        </div>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
